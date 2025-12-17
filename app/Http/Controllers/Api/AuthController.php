ebb98d8b5fb61702252708c79f2212a5915c2e31
ShafnatFR <shafnatfuainiramadhan@gmail.com>
2025-12-17 12:46:12 +0700

    implement authentication system with registration, login, and logout endpoints

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|confirmed|min:6',
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'succes' => false,
+                'message' => 'Input is not valid',
+                'errors' => $e->errors(),
+
+            ], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'user',
+        ]);
+
+        return response()->json([
+            'message' => 'Registration succes',
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        try {
+            $credentials = $request->validate([
+                'email' => 'required|email|',
+                'password' => 'required',
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'succes' => false,
+                'message' => 'Input is not valid',
+                'errors' => $e->errors(),
+
+            ], 422);
+        }
+
+        if (Auth::attempt($credentials)) {
+            
+            $user = Auth::user();
+            $token = $user->createToken('api_token')->plainTextToken;
+
+            return response()->json([
+                'token' => $token,
+                'message' => 'Login succes',
+            ], 200);
+        }
+
+        return response()->json(['message' => 'Invalid credential'], 401);
+    }
+
+    public function logout(Request $request){
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+        return response()->json(['message' => 'Logged out. Token deleted'], 200);
+    }
+}
